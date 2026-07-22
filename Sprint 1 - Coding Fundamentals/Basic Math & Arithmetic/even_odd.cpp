d7b8efd36800ca840f78843c6b755b7a47309467
S. Choudhary <sakshichoudhary5533@gmail.com>
2026-07-22 18:45:18 +0530

    Solve Q003 - Even Odd Check

---
+++ Sprint 1 - Coding Fundamentals/Basic Math & Arithmetic/even_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"Enter a number: ";
+    cin>>N;
+    if(N%2==0){
+        cout<<"Number is Even";
+    }
+    else{
+        cout<<"Number is Odd";
+    }
+}
