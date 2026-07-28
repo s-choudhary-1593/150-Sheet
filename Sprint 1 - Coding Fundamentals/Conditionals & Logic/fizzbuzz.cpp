9f1b065d7f4b31ba639c1217b5f67bcc4aa06ba3
S. Choudhary <s.choudhary1593@gmail.com>
2026-07-28 19:33:27 +0530

    Solve Q023 - FizzBuzz

---
+++ Sprint 1 - Coding Fundamentals/Conditionals & Logic/fizzbuzz.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Enter a number: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 15 == 0)
+        {
+            cout << "FizzBuzz"<<" ";
+        }
+        else if (i % 3 == 0)
+        {
+            cout << "Fizz"<<" ";
+        }
+        else if (i % 5 == 0)
+        {
+            cout << "Buzz"<<" ";
+        }
+        else
+        {
+            cout << i<<" ";
+        }
+    }
+}
